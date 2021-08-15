539e92f1e75ff6679eead308c14d2cba4a5a76f2
cherieeze <cherieeze@gmail.com>
2021-08-15 02:02:49 +0100

    Initial Commit

---
+++ Shared/FoodPing2App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPing2App.swift
+//  Shared
+//
+//  Created by c h e r i e on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodPing2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
